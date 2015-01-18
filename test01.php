e93d522a136aa0cf95ece930e513a0992aa306e9
hide1100 <hide.0822.leo.orion@gmail.com>
2015-01-18 10:59:01 +0900

    Update test01.php

--- test01.php
+++ test01.php
@@ -1,5 +1,5 @@
 <?php
-$json_string = file_get_contents(http://vachat4relay.herokuapp.com/botlist);
+$json_string = file_get_contents('http://vachat4relay.herokuapp.com/botlist');
 
 echo $json_string;
 $obj = json_decode($json_string);
