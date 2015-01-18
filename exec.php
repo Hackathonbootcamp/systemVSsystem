694f7392e93ac68d1c1e0447feaf428a6c791e02
hide1100 <hide.0822.leo.orion@gmail.com>
2015-01-18 23:58:36 +0900

    Update exec.php

--- exec.php
+++ exec.php
@@ -12,7 +12,7 @@
   $startUrl = "http://vachat4relay.herokuapp.com/start?bot_id1=".$botId1."&bot_id2=".$botId2."&start=".$startWord."&goal=".$endWord;
 echo $startUrl;
 
-　$jsonString = file_get_contents($startUrl);
+　$jsonString = file_get_contents("$startUrl");
   $aryResult = json_decode($jsonString);
   
   var_dump($aryResult);
