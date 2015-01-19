e4059fe6ca045b3de1fc05c92b7e8daa7a23cd75
hide1100 <hide.0822.leo.orion@gmail.com>
2015-01-19 23:52:36 +0900

    Update exec.php

--- exec.php
+++ exec.php
@@ -1,26 +1,23 @@
 <?php
   $startWord = $_POST['start_word'];
-  $endWord   = $_POST['end_word'];
-  
+  $EndWord   = $_POST['end_word'];
+
   echo $startWord;
-  echo $endWord;
-  
+  echo $EndWord;
+
   $botId1 = 1;
   $botId2 = 2;
-  
+
   // ゲームスタート
   $startUrl = "http://vachat4relay.herokuapp.com/start?bot_id1=".$botId1."&bot_id2=".$botId2."&start=".$startWord."&goal=".$endWord;
-echo $startUrl;
+　$jsonString = file_get_contents($startUrl);
+  $aryResult = json_decode($jsonString);
 
-　//$jsonString = file_get_contents("$startUrl");
-  //$aryResult = json_decode($jsonString);
-  
-  //var_dump($aryResult);
-  
-/*
+  var_dump($aryResult);
+
   if (is_array($aryResult)) {
-   $gameId = $aryResult['game_id'];
-   $run    = $aryResult['run?'];
+    $gameId = $aryResult['game_id'];
+    $run    = $aryResult['run'];
   }
   var_dump($aryResult);
   if ($run) {
@@ -30,5 +27,5 @@ echo $startUrl;
   }
   echo $retunUrl;
   //header("Location: $retunUrl");
- */ 
+
 ?>
