de9126c5dc09cdec949cac8545c1955d932565ea
hide1100 <hide.0822.leo.orion@gmail.com>
2015-01-20 22:32:13 +0900

    Update exec.php

--- exec.php
+++ exec.php
@@ -1,31 +1,33 @@
 <?php
-  $startWord = $_POST['start_word'];
-  $EndWord   = $_POST['end_word'];
+header("Content-type: text/html; charset=utf-8");
 
-  echo $startWord;
-  echo $EndWord;
+$startWord = $_POST['start_word'];
+$EndWord   = $_POST['end_word'];
 
-  $botId1 = 1;
-  $botId2 = 2;
+echo $startWord;
+echo $EndWord;
 
-  // ゲームスタート
-  $startUrl = "http://vachat4relay.herokuapp.com/start?bot_id1=".$botId1."&bot_id2=".$botId2."&start=".$startWord."&goal=".$endWord;
-　$jsonString = file_get_contents($startUrl);
-  $aryResult = json_decode($jsonString);
+$botId1 = 1;
+$botId2 = 2;
 
-  var_dump($aryResult);
+// ゲームスタート
+$startUrl = "http://vachat4relay.herokuapp.com/start?bot_id1=".$botId1."&bot_id2=".$botId2."&start=".$startWord."&goal=".$endWord;
+$jsonString = file_get_contents($startUrl);
+$aryResult = json_decode($jsonString);
 
-  if (is_array($aryResult)) {
-    $gameId = $aryResult['game_id'];
-    $run    = $aryResult['run'];
-  }
-  var_dump($aryResult);
-  if ($run) {
-    $retunUrl = "game_exec.php";
-  } else {
-    $retunUrl = "game_error.php";
-  }
-  echo $retunUrl;
-  //header("Location: $retunUrl");
+var_dump($aryResult);
+
+if (is_array($aryResult)) {
+  $gameId = $aryResult['game_id'];
+  $run    = $aryResult['run?'];
+}
+
+if ($run) {
+  $retunUrl = "game_exec.php";
+} else {
+  $retunUrl = "game_error.php";
+}
+echo $retunUrl;
+//header("Location: $retunUrl");
 
 ?>
