200ed245cd48a788ed394494eb11bcd6fd9124eb
hide1100 <hide.0822.leo.orion@gmail.com>
2015-01-21 23:57:10 +0900

    Update game.php

--- game.php
+++ game.php
@@ -16,14 +16,35 @@ $dbname = "d76k2v0lvos1l3";
 $user = "ollzkkdgygzkti";
 $pass = "3fdgK_t5FBW2n4-yGs5_D6Xh8f";
 
+$gameId = $_GET['game_id'];
+echo $gameId."<br />";
+
 $dba = pg_connect("host=$host dbname=$dbname user=$user password=$pass");
+
 if (!$dba) {
     die('接続失敗(ToT)'.pg_last_error());
 }
-print('接続に成功!<br>');
+
+$sql = "select id, word picture_url from chat_log where game_id = " . $gameId . " order by ins_time desc";
+$rs = pg_query($dba, $sql);
 ?>
 <body nowrap="" bgcolor="#ffffff" text="#333333">
-<div class="question_Box">
+<?php
+while ($row = pg_fetch_array($rs)) {
+  $className = "bot".$row['bot_id'];
+  $word = $row['word'];
+  $imgUrl = $row['word'];
+?>
+<div class="bot_Box">
+<div class="question_image"><img src="http://stat.ameba.jp/user_images/20140923/20/muse-baba/0b/60/j/t02200193_0300026313076027133.jpg" alt="" width="90" height="90"/></div>
+<div class="arrow_question">
+     ここに会話内容
+</div><!-- /.arrow_question -->
+</div><!-- /.question_Box -->
+<?php } ?>
+
+<!--
+<div class="bot_Box">
 <div class="question_image"><img src="http://stat.ameba.jp/user_images/20140923/20/muse-baba/0b/60/j/t02200193_0300026313076027133.jpg" alt="" width="90" height="90"/></div>
 <div class="arrow_question">
      ここに会話内容
@@ -36,5 +57,6 @@ print('接続に成功!<br>');
      ここに会話内容
 </div><!-- /.arrow_answer -->
 </div><!-- /.question_Box -->
+-->
 </body>
 </html>
