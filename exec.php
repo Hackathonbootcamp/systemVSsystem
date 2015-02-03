33f9b0e050dcfee60bc48ee7440a2c2d06e3030b
hide1100 <hide.0822.leo.orion@gmail.com>
2015-02-04 00:48:16 +0900

    Update exec.php

--- exec.php
+++ exec.php
@@ -6,8 +6,15 @@ $endWord   = $_POST['end_word'];
 $botId1 = $_POST['bot_id1'];
 $botId2 = $_POST['bot_id2'];
 
+$mode = $_POST['mode'];
+
+
 // ゲームスタート
-$startUrl = "http://vachat4relay.herokuapp.com/start?bot_id1=".$botId1."&bot_id2=".$botId2."&start=".$startWord."&goal=".$endWord;
+if ($mode == 1) {
+  $startUrl = "http://vachat4relay.herokuapp.com/start?bot_id1=".$botId1."&bot_id2=".$botId2."&start=".$startWord."&goal=".$endWord;
+} else {
+  $startUrl = "http://vachat4relay.herokuapp.com/multi?start=".$startWord."&goal=".$endWord;
+}
 $jsonString = file_get_contents($startUrl);
 $aryResult = json_decode($jsonString, true);
 var_dump($aryResult);
